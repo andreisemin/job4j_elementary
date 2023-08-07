da8111dc827b0a0fdbd94598dea3ef9e04a63518
andreisemin <andr-syomin@yandex.ru>
2023-08-07 23:24:10 +0400

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,13 @@ public class ArrayDefinition {
         System.out.println("Размер массива ages равен: " + ages.length);
         System.out.println("Размер массива surnames равен: " + surnames.length);
         System.out.println("Размер массива prices равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Petr";
+        names[1] = "Andrey";
+        names[2] = "Igor";
+        names[3] = "Sergey";
+        for (int i = 0; i < 4; i++) {
+            System.out.println("Имя ячейки " + (i + 1) + " : " + names[i]);
+        }
     }
 }
